faf60cf73dfe59dc84caa4dfa2f3a4a26ddc6838
Tanveer2719 <ar137375@gmail.com>
2022-11-25 16:05:04 +0600

    draft completed

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include"SymbolTable.cpp"
+using namespace std;
+
+int main(){
+    SymbolTable symbol_table(5);
+    symbol_table.enter_scope();
+    symbol_table.enter_scope();
+    
+    
+
+
+     
+    
+}
---
+++ SymbolTable.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<string>
+#include"ScopeTable.cpp"
+
+using namespace std;
+
+class SymbolTable{
+    // pointer for current scope
+    ScopeTable* current_scope;
+    int scope_count;
+    int MAX_SIZE;
+public:
+    SymbolTable()
+    {
+        current_scope = nullptr;
+        scope_count = 0;
+        MAX_SIZE = 0;
+    }
+    SymbolTable(int n){
+        MAX_SIZE = n;
+        scope_count = 1;
+        current_scope = new ScopeTable(MAX_SIZE);
+        current_scope->set_id(scope_count);
+        cout<<"main scope "<<current_scope->get_id()<<" created successfully"<<endl;
+    }
+    ~SymbolTable(){
+        delete current_scope;
+    }
+
+    void enter_scope(){
+        ScopeTable *temp = new ScopeTable(MAX_SIZE);
+        temp->set_parent(current_scope);
+        temp->set_id(++scope_count);
+        current_scope = temp;
+
+        cout<<"new scope "<<current_scope->get_id()<<" created successfully"<<endl;
+    }
+    void exit_scope(){
+        current_scope = current_scope->get_parent();
+        cout<<"current scope exitted successfully "<<endl;   
+    }
+
+    bool insert(string name, string type){
+        current_scope->insert(name, type);
+        cout<<"inserted in current scope successfully "<<endl;  
+    }
+
+    bool remove(string name){
+
+    }
+    SymbolInfo* look_up(string name){
+        ScopeTable* temp = current_scope;
+        while(temp){
+            SymbolInfo* ans = temp->look_up(name);
+            if(ans){
+                return ans;
+            }
+            temp = temp->get_parent();
+        }
+    }
+    void print_current_scope(){
+        current_scope->print();
+    }
+    void print_all_scopes(){
+        ScopeTable* temp = current_scope;
+        while(temp){
+            temp->print();
+            cout<<endl;
+            temp = temp->get_parent();
+        }
+    }
+
+
+};
+
